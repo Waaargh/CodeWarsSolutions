6de3e2668ebf1d9efa224c9887813a9cff2ffba9
Waaargh <Legion007@bk.ru>
2022-05-26 16:56:34 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,9 +7,6 @@ namespace CodeWarsSolutions
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Pasha!");
-            Console.WriteLine("Lol");
-            Console.WriteLine("Lol2");
-            Console.WriteLine("Lol3");
             Console.ReadLine();
         }
     }
