4eea550a1857254a446d7e4913b4f14fb2a6de1f
Waaargh <Legion007@bk.ru>
2022-05-26 16:41:31 +0300

    Lol

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace CodeWarsSolutions
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Pasha!");
+            Console.WriteLine("Lol");
+            Console.ReadLine();
         }
     }
 }
